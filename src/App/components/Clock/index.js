57f4a34a158c0c7b0e56a2e79706bb09cdc233b2
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-02 11:01:10 -0400

    Create timer function

--- src/App/components/Clock/index.js
+++ src/App/components/Clock/index.js
@@ -6,11 +6,56 @@ import ResetTimer from './components/Reset';
 import './index.css';
 
 class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: {
+        minutes: 0,
+        seconds: 0
+      },
+      timeTotal: 0
+    };
+
+    this.startTimer = this.startTimer.bind(this);
+  }
+
+  startTimer() {
+    this.makeInterval = setInterval(() => {
+      let { timeTotal } = this.state;
+      let { time } = this.state;
+
+      timeTotal += 1;
+      let minutes = Math.trunc(timeTotal / 60);
+      let seconds = timeTotal  % 60;
+
+      this.setState({
+        timeTotal: timeTotal
+      });
+      this.setState({
+        time: {
+          minutes: minutes,
+          seconds: seconds
+        }
+      });
+
+      // when reaching 0, clear the interval
+      if (timeTotal === 0) {
+        clearInterval(this.makeInterval);
+      }
+    }, 1000);
+  }
+
   render() {
     return(
       <div className="Clock">
+        <div>
+          <p>Minutes:  {this.state.time.minutes}</p>
+          <p>Seconds: {this.state.time.seconds}</p>
+          <p>Total: {this.state.timeTotal}</p>
+        </div>
         <StartStop />
         <ResetTimer />
+        <button onClick={this.startTimer}>Start timer</button>
       </div>
     );
   }
